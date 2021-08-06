fde56be57548387a9a744955ed5f3ea4166c5e2e
Sean McKay <swmckay94@gmail.com>
2021-08-06 10:13:40 +1000

    functionality update, continued progress

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,3 +2,57 @@
 const db = require('../db/db.json');
 const fs = require('fs');
 const path = require('path');
+
+
+module.exports = (app) => {
+    
+    app.get("/api/notes", (req, res) => {
+        console.log("Retrieving notes");
+        fs.readFile("db/db.json", "utf8", (err, data) => {
+            if (err) throw err;
+            let notes = JSON.parse(data);
+            res.json(notes);
+        });
+    });
+
+    app.post("/api/notes", (req, res) => {
+        fs.readFile("db/db.json", "utf8", (err, data) => {
+            if (err) throw err;
+
+            let addNote = req.body;
+            let updateNotes = json.parse(data);
+
+            updateNotes.push(addNote);
+
+            fs.writeFileSync("db/db.json", json.stringify(updateNotes), "utf8", (err, data) => {
+                if (err) throw err;
+                console.log("Added new note");
+            });
+            res.json(updateNotes);
+        });
+    });
+        
+    app.delete("/api/notes/:id", (req, res) => {
+        fs.readFile("db/db.json", "utf8", (err, data) => {
+            if (err) throw err;
+            
+            let updateNotes = json.parse(data);
+            let noteIndex = updateNotes.findIndex(
+                note.id === req.params.id
+            ); if (noteIndex !== req.params.id) {
+                return res.status(404).end();
+            }
+
+            let deleteNote = updateNotes[noteIndex];
+            updateNotes.splice(noteIndex);
+
+            fs.writeFileSync("db/db.json", 
+            json.stringify(updateNotes), "utf8", (err, data) => {
+                if (err) throw err;
+                console.log("Note deleted");
+            });
+
+            red.status(200).send(deleteNote);
+        });
+    });
+};
